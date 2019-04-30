3050daffb91f5c80d3f0539d424a3ed9d4e993e1
chlsedu <cdgedu@qq.com>
2019-04-30 16:27:01 +0800

    2

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,20 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { ComponentsModule } from './components';
 
 @NgModule({
+  declarations: [],
   imports: [
-    CommonModule,
-    FormsModule,
-  ],
-  declarations: [
-  ],
-  exports: [
-    CommonModule,
-    FormsModule,
-    ComponentsModule
-  ],
-  entryComponents: [],
+    CommonModule
+  ]
 })
-export class SharedModule {}
+export class SharedModule { }
